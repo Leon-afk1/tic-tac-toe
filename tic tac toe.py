44ed0d0a16cf583fe958b0e8d01d5cb536a4d8cc
leon <leon.morales@utbm.fr>
2024-02-13 19:24:08 +0100

    Change langage to english

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,5 +1,4 @@
 import pygame
-import heapq
 
 class TicTacToe:
     def __init__(self):
@@ -10,27 +9,27 @@ class TicTacToe:
         self.background = pygame.image.load("OIP.jpeg")
         self.rect = self.background.get_rect(topleft=(0, 0))
 
-        self.etat_jeu = [['_'] * 3 for _ in range(3)]
+        self.game_state = [['_'] * 3 for _ in range(3)]
 
-        self.croix = pygame.image.load("croix.png")
-        self.croix = pygame.transform.scale(self.croix, (150, 150))
+        self.cross = pygame.image.load("cross.png")
+        self.cross = pygame.transform.scale(self.cross, (150, 150))
 
-        self.cercle = pygame.image.load("cercle.png")
-        self.cercle = pygame.transform.scale(self.cercle, (150, 150))
+        self.circle = pygame.image.load("circle.png")
+        self.circle = pygame.transform.scale(self.circle, (150, 150))
         
-        self.barre_hori = pygame.image.load("barre_hori.png")
-        self.barre_hori = pygame.transform.scale(self.barre_hori, (474, 200))
+        self.horiz_bar = pygame.image.load("horiz_bar.png")
+        self.horiz_bar = pygame.transform.scale(self.horiz_bar, (474, 200))
         
-        self.barre_verti = pygame.image.load("barre_verti.png")
-        self.barre_verti = pygame.transform.scale(self.barre_verti, (200, 474))
+        self.vert_bar = pygame.image.load("vert_bar.png")
+        self.vert_bar = pygame.transform.scale(self.vert_bar, (200, 474))
         
-        self.barre_diag_droite = pygame.image.load("barre_diag_droite.png")
-        self.barre_diag_droite = pygame.transform.scale(self.barre_diag_droite, (474, 474))
+        self.diag_right_bar = pygame.image.load("diag_right_bar.png")
+        self.diag_right_bar = pygame.transform.scale(self.diag_right_bar, (474, 474))
         
-        self.barre_diag_gauche = pygame.image.load("barre_diag_gauche.png")
-        self.barre_diag_gauche = pygame.transform.scale(self.barre_diag_gauche, (474, 474))
+        self.diag_left_bar = pygame.image.load("diag_left_bar.png")
+        self.diag_left_bar = pygame.transform.scale(self.diag_left_bar, (474, 474))
 
-        self.cases_rect = [
+        self.cell_rects = [
             pygame.Rect(0, 154, 150, 150),
             pygame.Rect(160, 154, 150, 150),
             pygame.Rect(320, 154, 150, 150),
@@ -42,69 +41,69 @@ class TicTacToe:
             pygame.Rect(320, 464, 150, 150)
         ]
         
-        self.conditions_gagnantes = {
-            ((0, 0), (0, 1), (0, 2)): "barre1",
-            ((1, 0), (1, 1), (1, 2)): "barre2",
-            ((2, 0), (2, 1), (2, 2)): "barre3",
-            ((0, 0), (1, 0), (2, 0)): "barre4",
-            ((0, 1), (1, 1), (2, 1)): "barre5",
-            ((0, 2), (1, 2), (2, 2)): "barre6",
-            ((0, 0), (1, 1), (2, 2)): "diagonal anti slash",
-            ((0, 2), (1, 1), (2, 0)): "diagonal slash"
+        self.winning_conditions = {
+            ((0, 0), (0, 1), (0, 2)): "horiz_bar1",
+            ((1, 0), (1, 1), (1, 2)): "horiz_bar2",
+            ((2, 0), (2, 1), (2, 2)): "horiz_bar3",
+            ((0, 0), (1, 0), (2, 0)): "vert_bar1",
+            ((0, 1), (1, 1), (2, 1)): "vert_bar2",
+            ((0, 2), (1, 2), (2, 2)): "vert_bar3",
+            ((0, 0), (1, 1), (2, 2)): "diag_left",
+            ((0, 2), (1, 1), (2, 0)): "diag_right"
         }
         
-        self.value_barre = "vide"
+        self.winning_bar = "empty"
         
-        self.barre_completee_pos = None
+        self.complete_bar_pos = None
 
         self.running = True
-        self.tour = 0
+        self.turn = 0
 
-    def afficher(self):
+    def display(self):
         self.screen.blit(self.background, self.rect.topleft)
 
-        for i, rect in enumerate(self.cases_rect):
+        for i, rect in enumerate(self.cell_rects):
             x, y = rect.topleft
-            if self.etat_jeu[i // 3][i % 3] == 'X':
-                self.screen.blit(self.croix, (x, y))
-            elif self.etat_jeu[i // 3][i % 3] == 'O':
-                self.screen.blit(self.cercle, (x, y))
+            if self.game_state[i // 3][i % 3] == 'X':
+                self.screen.blit(self.cross, (x, y))
+            elif self.game_state[i // 3][i % 3] == 'O':
+                self.screen.blit(self.circle, (x, y))
                 
-        if self.barre_completee_pos is not None:
-            self.afficher_barre_completee()
+        if self.complete_bar_pos is not None:
+            self.display_complete_bar()
 
         pygame.display.flip()
         
-    def afficher_barre_completee(self):
-        if self.value_barre != "vide":
-            if self.value_barre == "barre1" :
-                self.screen.blit(self.barre_hori, (0, 130))
-            elif self.value_barre == "barre2" :
-                self.screen.blit(self.barre_hori, (0, 290))
-            elif self.value_barre == "barre3" :
-                self.screen.blit(self.barre_hori, (0, 440))
-            elif self.value_barre == "barre4" :
-                self.screen.blit(self.barre_verti, (-20, 150))
-            elif self.value_barre == "barre5" :
-                self.screen.blit(self.barre_verti, (140, 150))
-            elif self.value_barre == "barre6" :
-                self.screen.blit(self.barre_verti, (290, 150))
-            elif self.value_barre == "diagonal anti slash" :
-                self.screen.blit(self.barre_diag_gauche, (0, 150))
-            elif self.value_barre == "diagonal slash" :
-                self.screen.blit(self.barre_diag_droite, (0, 150))
+    def display_complete_bar(self):
+        if self.winning_bar != "empty":
+            if self.winning_bar == "horiz_bar1" :
+                self.screen.blit(self.horiz_bar, (0, 130))
+            elif self.winning_bar == "horiz_bar2" :
+                self.screen.blit(self.horiz_bar, (0, 290))
+            elif self.winning_bar == "horiz_bar3" :
+                self.screen.blit(self.horiz_bar, (0, 440))
+            elif self.winning_bar == "vert_bar1" :
+                self.screen.blit(self.vert_bar, (-20, 150))
+            elif self.winning_bar == "vert_bar2" :
+                self.screen.blit(self.vert_bar, (140, 150))
+            elif self.winning_bar == "vert_bar3" :
+                self.screen.blit(self.vert_bar, (290, 150))
+            elif self.winning_bar == "diag_left" :
+                self.screen.blit(self.diag_left_bar, (0, 150))
+            elif self.winning_bar == "diag_right" :
+                self.screen.blit(self.diag_right_bar, (0, 150))
 
     
-    def afficher_resultat(self, resultat):
-        phrase = "Victoire de " + resultat if resultat != "Match nul" else "Match nul"
+    def display_result(self, result):
+        phrase = "Victory for " + result if result != "Draw" else "Draw"
         
         pygame.time.wait(2000)
         self.screen.fill(self.background_colour)
         
         font = pygame.font.Font(None, 36)
-        texte = font.render(phrase, True, (0, 0, 0))
-        rect = texte.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
-        self.screen.blit(texte, rect.topleft)
+        text = font.render(phrase, True, (0, 0, 0))
+        rect = text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
+        self.screen.blit(text, rect.topleft)
         pygame.display.flip()
 
     def events(self):
@@ -112,98 +111,98 @@ class TicTacToe:
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if self.tour == 0:
+                if self.turn == 0:
                     self.on_mouse_down()
 
     def on_mouse_down(self):
         x, y = pygame.mouse.get_pos()
-        for i, rect in enumerate(self.cases_rect):
-            if rect.collidepoint(x, y) and self.etat_jeu[i // 3][i % 3] == '_':
-                self.jouer_coup(i)
-                self.jouer_coup_ordinateur()
+        for i, rect in enumerate(self.cell_rects):
+            if rect.collidepoint(x, y) and self.game_state[i // 3][i % 3] == '_':
+                self.make_move(i)
+                self.make_computer_move()
                 return  
 
-    def jouer_coup(self, position):
-        interieur = 'O' if self.tour == 1 else 'X'
-        self.etat_jeu[position // 3][position % 3] = interieur
-        self.tour = (self.tour + 1) % 2
-        self.afficher_etat_jeu()
+    def make_move(self, position):
+        symbol = 'O' if self.turn == 1 else 'X'
+        self.game_state[position // 3][position % 3] = symbol
+        self.turn = (self.turn + 1) % 2
+        self.display_game_state()
         
 
-    def jouer_coup_ordinateur(self):
-        meilleur_score = float('-inf')
-        meilleur_action = None
+    def make_computer_move(self):
+        best_score = float('-inf')
+        best_action = None
 
         for i in range(9):
-            if self.etat_jeu[i // 3][i % 3] == '_':
-                self.etat_jeu[i // 3][i % 3] = 'O'
+            if self.game_state[i // 3][i % 3] == '_':
+                self.game_state[i // 3][i % 3] = 'O'
                 score = self.minimax(False)
-                self.etat_jeu[i // 3][i % 3] = '_'
+                self.game_state[i // 3][i % 3] = '_'
 
-                if score > meilleur_score:
-                    meilleur_score = score
-                    meilleur_action = i
+                if score > best_score:
+                    best_score = score
+                    best_action = i
 
-        if meilleur_action is not None:
-            self.jouer_coup(meilleur_action)
+        if best_action is not None:
+            self.make_move(best_action)
 
 
-    def evaluer(self):
-        resultat = self.check_victoire()
-        if resultat == 'X':
+    def evaluate(self):
+        result = self.check_victory()
+        if result == 'X':
             return -1
-        elif resultat == 'O':
+        elif result == 'O':
             return 1
-        elif resultat == 'Match nul':
+        elif result == 'Draw':
             return 0
         else:
             return None
 
     
-    def minimax(self, est_maximisant):
-        score = self.evaluer()
+    def minimax(self, is_maximizing):
+        score = self.evaluate()
 
         if score is not None:
             return score
 
-        if est_maximisant:
-            meilleur_score = float('-inf')
+        if is_maximizing:
+            best_score = float('-inf')
             for i in range(9):
-                if self.etat_jeu[i // 3][i % 3] == '_':
-                    self.etat_jeu[i // 3][i % 3] = 'O'
-                    meilleur_score = max(meilleur_score, self.minimax(False))
-                    self.etat_jeu[i // 3][i % 3] = '_'
-            return meilleur_score
+                if self.game_state[i // 3][i % 3] == '_':
+                    self.game_state[i // 3][i % 3] = 'O'
+                    best_score = max(best_score, self.minimax(False))
+                    self.game_state[i // 3][i % 3] = '_'
+            return best_score
         else:
-            meilleur_score = float('inf')
+            best_score = float('inf')
             for i in range(9):
-                if self.etat_jeu[i // 3][i % 3] == '_':
-                    self.etat_jeu[i // 3][i % 3] = 'X'
-                    meilleur_score = min(meilleur_score, self.minimax(True))
-                    self.etat_jeu[i // 3][i % 3] = '_'
-            return meilleur_score
+                if self.game_state[i // 3][i % 3] == '_':
+                    self.game_state[i // 3][i % 3] = 'X'
+                    best_score = min(best_score, self.minimax(True))
+                    self.game_state[i // 3][i % 3] = '_'
+            return best_score
 
-    def check_victoire(self):
-        for positions, nom_barre in self.conditions_gagnantes.items():
-            if all(self.etat_jeu[i][j] == 'O' for i, j in positions):
-                self.value_barre = nom_barre
-                self.barre_completee_pos=(1,1)
+    def check_victory(self):
+        for positions, bar_name in self.winning_conditions.items():
+            if all(self.game_state[i][j] == 'O' for i, j in positions):
+                self.winning_bar = bar_name
+                self.complete_bar_pos=(1,1)
                 return 'O'
-            elif all(self.etat_jeu[i][j] == 'X' for i, j in positions):
-                self.value_barre = nom_barre
-                self.barre_completee_pos=(1,1)
+            elif all(self.game_state[i][j] == 'X' for i, j in positions):
+                self.winning_bar = bar_name
+                self.complete_bar_pos=(1,1)
                 return 'X'
 
-        if all(self.etat_jeu[i][j] != '_' for i in range(3) for j in range(3)):
-            return "Match nul"
+        if all(self.game_state[i][j] != '_' for i in range(3) for j in range(3)):
+            return "Draw"
 
-        self.value_barre = "vide"
+        self.winning_bar = "empty"
         return '_'
 
     
-    def afficher_etat_jeu(self):
+    def display_game_state(self):
         for i in range(3):
-            print(self.etat_jeu[i])
+            print(self.game_state[i])
             
         print()
             
@@ -211,30 +210,30 @@ class TicTacToe:
     def run(self):
         while self.running:
             self.events()
-            resultat = self.check_victoire()
-            self.afficher()
+            result = self.check_victory()
+            self.display()
 
             
-            if resultat != "_":
-                if resultat == "Match nul":
-                    print("Match nul")
+            if result != "_":
+                if result == "Draw":
+                    print("Draw")
                 else:
-                    print("Victoire de", resultat)
-                self.afficher_resultat(resultat)
+                    print("Victory for", result)
+                self.display_result(result)
                 
-                attente = True
-                while attente:
+                waiting = True
+                while waiting:
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             self.running = False
-                            attente = False
+                            waiting = False
                         elif event.type == pygame.MOUSEBUTTONDOWN:
-                            attente = False
+                            waiting = False
                 
                 self.__init__()
                 
         pygame.quit()
 
 
-jeu = TicTacToe()
-jeu.run()
+game = TicTacToe()
+game.run()
